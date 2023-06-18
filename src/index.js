1ce8c6a9458d94a1cafac4ea26b5d48a4e8cb30d
Rajan Chaudhary <rajann44@gmail.com>
2023-06-18 21:33:42 +0200

    react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ErrorPage from "./error-page";
+import Auth from "./components/Auth";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "auth",
+    element: <Auth />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar></Navbar>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
