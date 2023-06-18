42f6fb64da79b89daa06c702c9ea066fd18cf33e
Rajan Chaudhary <rajann44@gmail.com>
2023-06-19 00:06:36 +0200

    Home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/Auth";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home></Home>} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
